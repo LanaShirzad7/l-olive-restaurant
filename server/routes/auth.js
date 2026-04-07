15c375719d0e786d8e83c3547dd781ba01b5c3d0
Lana Shirzad <lana@Lanas-MacBook-Air.local>
2026-04-07 17:11:44 +0330

    Fix email

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -11,7 +11,7 @@ const transporter = nodemailer.createTransport({
   service: "gmail",
   auth: {
     user: "lolivekitchen@gmail.com",
-    pass: "eilicrlbgwzurseb",
+    pass: "bitucnumvvgpdxfy",
   },
 });
 
